b05d1d3f9f08663a88077be931a53a7f87c77caf
PabloRR100 <pabloruizruiz10@gmail.com>
2020-03-22 11:55:22 +0100

    comma was causing the tests to fail?

--- app/tests/movies/test_views.py
+++ app/tests/movies/test_views.py
@@ -2,6 +2,8 @@ import pytest
 from movies.models import Movie
 
 
+""" CREATE """
+
 @pytest.mark.django_db
 def test_add_movie(client):
     movies = Movie.objects.all()
@@ -9,7 +11,7 @@ def test_add_movie(client):
 
     resp = client.post(
         "/api/movies/",
-        {"title": "The Big Lebowski", "genre": "comedy", "year": "1998",},
+        {"title": "The Big Lebowski", "genre": "comedy", "year": "1998"},
         content_type="application/json",
     )
     assert resp.status_code == 201
@@ -47,6 +49,8 @@ def test_add_movie_invalid_json_keys(client):
     assert len(movies) == 0
 
 
+""" GET """
+
 @pytest.mark.django_db
 def test_get_single_movie(client, add_movie):
     movie = add_movie(title="The Big Lebowski", genre="comedy", year="1998")
@@ -68,3 +72,73 @@ def test_get_all_movies(client, add_movie):
     resp = client.get(f"/api/movies/")
     assert resp.data[0]["title"] == movie1.title
     assert resp.data[1]["title"] == movie2.title
+
+
+""" DELETE """
+
+@pytest.mark.django_db
+def test_remove_movie(client, add_movie):
+    movie = add_movie(title="The Big Lebowski", genre="comedy", year="1998")
+
+    resp = client.get(f"/api/movies/{movie.id}/")
+    assert resp.status_code == 200
+    assert resp.data["title"] == "The Big Lebowski"
+
+    resp_two = client.delete(f"/api/movies/{movie.id}/")
+    assert resp_two.status_code == 204
+
+    resp_three = client.get("/api/movies/")
+    assert resp_three.status_code == 200
+    assert len(resp_three.data) == 0
+
+
+@pytest.mark.django_db
+def test_remove_movie_incorrect_id(client):
+    resp = client.delete(f"/api/movies/99/")
+    assert resp.status_code == 404
+
+
+""" UPDATE """
+
+@pytest.mark.django_db
+def test_update_movie(client, add_movie):
+    movie = add_movie(title="The Big Lebowski", genre="comedy", year="1998")
+
+    resp = client.put(
+        f"/api/movies/{movie.id}/",
+        {"title": "The Big Lebowski", "genre": "comedy", "year": "1997"},
+        content_type="application/json"
+    )
+    assert resp.status_code == 200
+    assert resp.data["title"] == "The Big Lebowski"
+    assert resp.data["year"] == "1997"
+
+    resp_two = client.get(f"/api/movies/{movie.id}/")
+    assert resp_two.status_code == 200
+    assert resp_two.data["title"] == "The Big Lebowski"
+    assert resp.data["year"] == "1997"
+
+
+@pytest.mark.django_db
+def test_update_movie_incorrect_id(client):
+    resp = client.put(f"/api/movies/99/")
+    assert resp.status_code == 404
+
+
+@pytest.mark.django_db
+def test_update_movie_invalid_json(client, add_movie):
+    movie = add_movie(title="The Big Lebowski", genre="comedy", year="1998")
+    resp = client.put(f"/api/movies/{movie.id}/", {}, content_type="application/json")
+    assert resp.status_code == 400
+
+
+@pytest.mark.django_db
+def test_update_movie_invalid_json_keys(client, add_movie):
+    movie = add_movie(title="The Big Lebowski", genre="comedy", year="1998")
+
+    resp = client.put(
+        f"/api/movies/{movie.id}/",
+        {"title": "The Big Lebowski", "genre": "comedy"},
+        content_type="application/json",
+    )
+    assert resp.status_code == 400
